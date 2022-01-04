1c21c9700824685b541baf9a3b4a8b0736f62951
lakshman.narale <lakshman.narale@collabera.com>
2022-01-04 12:53:14 +0530

    first commit : javaBasicTest.java

---
+++ java-simple-app/src/test/java/javaBasicTest.java
@@ -0,0 +1,18 @@
+public class javaBasicTest{
+    public void testVariable()
+{
+    int age=10;
+  float sal = 28.6f;
+  double annualSal = 56.56;
+  char ch='a';
+  boolean isManager = false;
+  byte byt=10;
+  short sh=10;
+  long lng=4646464646464L;
+  String nm = "android";
+}
+public void testMethod(){
+
+}
+}
+
