0310c98ea4bce220e0f0c328aa98bb19acd4c2e8
lakshman.narale <lakshman.narale@collabera.com>
2022-01-04 18:06:41 +0530

    learning Inheritance

--- java-simple-app/src/main/java/clsobj/Bmw.java
+++ java-simple-app/src/main/java/clsobj/Bmw.java
@@ -1,4 +1,6 @@
 package clsobj;
-
+// bmw is a car
+// -> object of bmw should be object of car
+// -> object of car is not the object of bmw
 public class Bmw  extends Car  {
 }
--- java-simple-app/src/test/java/BmwTests.java
+++ java-simple-app/src/test/java/BmwTests.java
@@ -1,10 +1,11 @@
 import clsobj.Bmw;
+import clsobj.Car;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BmwTests
-{
+public class BmwTests {
+
     @DisplayName("Testing Object Nullability")
     @Test
     void testInValidObject() {
@@ -22,5 +23,23 @@ public class BmwTests
         int expectedSpeed = 10;
         int actualSpeed = bmw.speed;
         Assertions.assertEquals(expectedSpeed, actualSpeed);
+    }
+
+    @DisplayName("Test is Bmw is a Car")
+    @Test
+    void testBmwObjectCar() {
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof Car;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw() {
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
     }
 }
