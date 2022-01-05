62843a64a9d82e8ff498dd46a4d3901f06ddca16
lakshman.narale <lakshman.narale@collabera.com>
2022-01-05 11:30:21 +0530

    Exception Handling

--- Day3/src/main/java/oop/DivisionOfTwoNum.java
+++ Day3/src/main/java/oop/DivisionOfTwoNum.java
@@ -2,10 +2,15 @@ package oop;
 
 public class DivisionOfTwoNum {
 
-    public int division(int num1 , int num2){
-
-       return num1/num2;
-   }
-
+    public int division(int num1, int num2) {
+        if (num2 > 0 && num1 > 0)
+            return num1 / num2;
+        return 0;
     }
+
+    public double divideNumbersException(double num1, double num2) {
+        if (num2 == 0) throw new ArithmeticException("num2 is 0");
+        return num1 / num2; // throw new ArithmeticException()
+    }
+}
 
--- Day3/src/test/java/DivisionOfTwoNumTest.java
+++ Day3/src/test/java/DivisionOfTwoNumTest.java
@@ -1,11 +1,40 @@
 import oop.DivisionOfTwoNum;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DivisionOfTwoNumTest {
-@Test
-    void divTest(){
-        DivisionOfTwoNum d=new DivisionOfTwoNum();
-        Assertions.assertNotNull(d);
+    @DisplayName("test for Complete Number")
+    @Test
+    void divTest() {
+        DivisionOfTwoNum d = new DivisionOfTwoNum();
+        var num1 = 10;
+        var num2 = 10;
+        var expected = 1;
+        var actual = d.division(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test for zero num")
+    @Test
+    void testunexpectedNumbers() {
+        var num1 = 4;
+        var num2 = 0;
+        DivisionOfTwoNum d = new DivisionOfTwoNum();
+        var expected = 0;
+        var actual = d.division(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Testing Exceptions")
+    @Test
+    void testUnexpectedNumbersWithException() {
+        var num1 = 4.5;
+        var num2 = 0;
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            var dvDr = new DivisionOfTwoNum();
+            dvDr.divideNumbersException(num1, num2);
+        });
     }
 }
