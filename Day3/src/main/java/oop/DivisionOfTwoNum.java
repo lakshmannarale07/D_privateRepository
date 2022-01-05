5e0bfc2c5b60793d14c5ae134538c7e0e730236a
lakshman.narale <lakshman.narale@collabera.com>
2022-01-05 10:41:40 +0530

    DivisionOfTwoNumber

---
+++ Day3/src/main/java/oop/DivisionOfTwoNum.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class DivisionOfTwoNum {
+
+    public int division(int num1 , int num2){
+
+       return num1/num2;
+   }
+
+    }
+
---
+++ Day3/src/test/java/DivisionOfTwoNumTest.java
@@ -0,0 +1,11 @@
+import oop.DivisionOfTwoNum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DivisionOfTwoNumTest {
+@Test
+    void divTest(){
+        DivisionOfTwoNum d=new DivisionOfTwoNum();
+        Assertions.assertNotNull(d);
+    }
+}
