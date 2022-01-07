523fe2848da85844c2be72d7696a8eed4763e25a
lakshman.narale <lakshman.narale@collabera.com>
2022-01-07 15:09:42 +0530

    abstract factory

--- day5/src/main/java/factory/Atm.java
+++ day5/src/main/java/factory/Atm.java
@@ -1,9 +1,16 @@
 package factory;
 
 public class Atm {
+
     public CardProvider swipe(String bank) {
         CardProvider card = CardFactory.getCard(bank);
         card.swipe(150);
         return card;
+    }
+
+    public CoinProvider pay(String bank) {
+        CoinProvider coin = CoinFactory.getCoin("abc");
+        coin.currentPrice();
+        return coin;
     }
 }
