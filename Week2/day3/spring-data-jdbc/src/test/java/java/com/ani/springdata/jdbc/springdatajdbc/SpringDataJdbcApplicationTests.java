d74587928d953e9d17ecc5feac58fbe3a204e456
lakshman.narale <lakshman.narale@collabera.com>
2022-01-12 13:16:41 +0530

    template jdbc setup

---
+++ Week2/day3/spring-data-jdbc/src/test/java/java/com/ani/springdata/jdbc/springdatajdbc/SpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.ani.springdata.jdbc.springdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
