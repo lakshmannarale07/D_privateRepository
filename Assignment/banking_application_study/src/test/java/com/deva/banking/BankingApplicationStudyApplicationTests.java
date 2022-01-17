696778b2e1f8935d304804b4c1be95724ac3732b
lakshman.narale <lakshman.narale@collabera.com>
2022-01-17 17:23:30 +0530

    microservice : inter service communication

---
+++ Assignment/banking_application_study/src/test/java/com/deva/banking/BankingApplicationStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deva.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
